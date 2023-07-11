ef147958547c6d7aacbc4dc5dea689f75a2f7297
rciochin <ciochina7@gmail.com>
2023-07-11 14:11:19 +0300

    activemq done

---
+++ camel-microservice-a/src/main/java/com/raduciochina/camelmicroservicea/routes/c/ActiveMqSenderRouter.java
@@ -0,0 +1,17 @@
+package com.raduciochina.camelmicroservicea.routes.c;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ActiveMqSenderRouter extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+        //timer  - 1st endpoint
+        from("timer:active-mq-timer?period=10000")
+                .transform().constant("My message for Active MQ")
+                .log("${body}")
+                .to("activemq:my-activemq-queue");
+        //queue  - 2nd endpoint
+    }
+}
